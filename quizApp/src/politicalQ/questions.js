ebfcf6663d608650d00e4ebcf8819bc3e6bf7cc5
Abiola <66026349+Webui-lab@users.noreply.github.com>
2023-09-04 05:19:26 +0100

    political quiz

---
+++ quizApp/src/politicalQ/questions.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: 0 //"Paris",
+  },
+  {
+    question: "What does Porphyrophile stands for?",
+    options: [
+      "Confidence",
+      "Love for color purple",
+      "Fear of color purple",
+      "Love for colors",
+    ],
+    correctAnswer: 1 //"Love for color purple",
+  },
+];
+
+export default questions;
