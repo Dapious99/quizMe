f5c64d10d8ee9f20426dddd4e7f3036a9933cb10
Abiola <dapious99@gmail.com>
2023-09-05 01:33:56 +0100

    three more questions

--- quizApp/src/politicalQ/questions.js
+++ quizApp/src/politicalQ/questions.js
@@ -16,8 +16,23 @@ const questions = [
   },
   {
     question: "What year is Berlin conference?",
-    options: ["1885", "1884", "Berlin", "Madrid"],
-    correctAnswer: 0 //"Paris",
+    options: ["1885", "1884", "1997", "1202"],
+    correctAnswer: 0 //"1885",
+  },
+  {
+    question: "The partioning of Africa and sharing it resources among European powers was done at ...",
+    options: ["UN Conference", "International Conference", "Berlin Conference", "WWII"],
+    correctAnswer: 2 //"Berlin Conference",
+  },
+  {
+    question: "The United Nations(UN) is preceded by which body?",
+    options: ["OAU(Organization of African Unity)", "WHO(World Health Organization)", "European Union (EU)", "League of Nations"],
+    correctAnswer: 3 //"League of Nations",
+  },
+  {
+    question: "The commonwealth countries are colonized by?",
+    options: ["Britain", "Germany", "Russia", "France"],
+    correctAnswer: 0 //"Britain",
   },
 ];
 
