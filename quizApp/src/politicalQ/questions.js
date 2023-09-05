57d9d27213b1bef56d1d8925f8b96edb6e37be57
Abiola <dapious99@gmail.com>
2023-09-05 06:28:14 +0100

    questions

--- quizApp/src/politicalQ/questions.js
+++ quizApp/src/politicalQ/questions.js
@@ -2,7 +2,42 @@ const questions = [
   {
     question: "What is the capital of France?",
     options: ["Paris", "London", "Berlin", "Madrid"],
-    correctAnswer: 0 //"Paris",
+    correctAnswer: 0, //"Paris",
+  },
+  {
+    question: "_____ was formely referred to as NyasaLand.",
+    options: ["Congo", "Seychelles", "Madagascar", "Malawi"],
+    correctAnswer: 3, //"Malawi",
+  },
+  {
+    question: "Nigeria gain her independence on October 1st ____?",
+    options: ["1914", "1960", "1956", "1963"],
+    correctAnswer: 1, //"1960",
+  },
+  {
+    question: "____ and ____ are countries in West Africa",
+    options: [
+      "Zimbabwe and Ghana",
+      "Sudan and Uganda",
+      "Sudan and South-Sudan",
+      "Chad and Guinea-Bissau",
+    ],
+    correctAnswer: 3, //"Chad and Guinea-Bissau",
+  },
+  {
+    question: "All these countries are North Africa countries except _____",
+    options: ["Algeria", "Mali", "Egpyt", "Morrocco"],
+    correctAnswer: 1, //"Mali",
+  },
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: 0, //"Paris",
+  },
+  {
+    question: "What is the capital of Sierra-Leone?",
+    options: ["Timbuktu", "London", "Freetown", "Lagos"],
+    correctAnswer: 2, //"Freetown",
   },
   {
     question: "What does Porphyrophile stands for?",
@@ -12,27 +47,39 @@ const questions = [
       "Fear of color purple",
       "Love for colors",
     ],
-    correctAnswer: 1 //"Love for color purple",
+    correctAnswer: 1, //"Love for color purple",
   },
+  {},
   {
-    question: "What year is Berlin conference?",
+    question: "What year is the Berlin conference?",
     options: ["1885", "1884", "1997", "1202"],
-    correctAnswer: 0 //"1885",
+    correctAnswer: 0, //"1885",
   },
   {
-    question: "The partioning of Africa and sharing it resources among European powers was done at ...",
-    options: ["UN Conference", "International Conference", "Berlin Conference", "WWII"],
-    correctAnswer: 2 //"Berlin Conference",
+    question:
+      "The partioning of Africa and sharing it resources among European powers was done at ...",
+    options: [
+      "UN Conference",
+      "International Conference",
+      "Berlin Conference",
+      "WWII",
+    ],
+    correctAnswer: 2, //"Berlin Conference",
   },
   {
     question: "The United Nations(UN) is preceded by which body?",
-    options: ["OAU(Organization of African Unity)", "WHO(World Health Organization)", "European Union (EU)", "League of Nations"],
-    correctAnswer: 3 //"League of Nations",
+    options: [
+      "OAU(Organization of African Unity)",
+      "WHO(World Health Organization)",
+      "European Union (EU)",
+      "League of Nations",
+    ],
+    correctAnswer: 3, //"League of Nations",
   },
   {
     question: "The commonwealth countries are colonized by?",
     options: ["Britain", "Germany", "Russia", "France"],
-    correctAnswer: 0 //"Britain",
+    correctAnswer: 0, //"Britain",
   },
 ];
 
