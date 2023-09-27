e1045467ef6529ba460b2faf09203145181ead22
Abiola <dapious99@gmail.com>
2023-09-27 04:45:11 +0100

    21 questions added

--- quizApp/src/App.jsx
+++ quizApp/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PoliticalQuiz from './politicalQ/PoliticalQuiz'
+import PoliticalQuiz from "./politicalQ/PoliticalQuiz";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <PoliticalQuiz />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
