2b2deb4033f919a4b9e2cce891725f6734636f50
Omotosho Kamalideen <dapious99@gmail.com>
2023-11-10 10:27:58 +0100

    responsiveness

--- quizApp/src/homepage/Home.jsx
+++ quizApp/src/homepage/Home.jsx
@@ -5,12 +5,15 @@ const Home = () => {
   return (
     <div className="flex justify-center items-center bg-gray-500 h-screen">
       <div>
-        <p className="animate__animated animate__flip animate__repeat-3 animate__slower font-semibold text-center py-4 text-[3rem]">
+        <p className="animate__animated animate__flip animate__repeat-3 animate__slower font-semibold sm:font-medium md:font-medium text-2xl text-center py-4 lg:text-[3rem]">
           quizMe
         </p>
-        <p className=" text-[2rem]">
-          Welcome to <span className="italic text-blue-700 text-[2.5rem]">quizMe</span>,
-          your friendly Quiz app.
+        <p className="lg:text-[2rem] text-2xl">
+          Welcome to{" "}
+          <span className="italic text-blue-700 text-[2rem] lg:text-[2.5rem]">
+            quizMe
+          </span>
+          , your friendly Quiz app.
         </p>
       </div>
     </div>
