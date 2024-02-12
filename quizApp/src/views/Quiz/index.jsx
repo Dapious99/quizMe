4b4928e933e9af256ead20efd4aad5eb61e2357b
Omotosho Kamalideen <dapious99@gmail.com>
2024-02-12 06:16:08 +0100

    cleanUp and adjustments in codebase

--- quizApp/src/views/Quiz/index.jsx
+++ quizApp/src/views/Quiz/index.jsx
@@ -1,5 +1,168 @@
+// import React, { useState } from "react";
+// import MissedQuestionsPage from "./MissedQuestionsPage";
+
+// function Quiz({ questions }) {
+//   const [currentPage, setCurrentPage] = useState("quiz");
+//   const [page, setPage] = useState(0);
+//   const questionsPerPage = 8;
+//   const totalPages = Math.ceil(questions.length / questionsPerPage);
+//   const startIndex = page * questionsPerPage;
+//   const endIndex = startIndex + questionsPerPage;
+//   const currentQuestions = questions.slice(startIndex, endIndex);
+//   const [selectedAnswers, setSelectedAnswers] = useState(
+//     new Array(questions.length).fill(null)
+//   );
+//   const [showScore, setShowScore] = useState(false);
+//   const [answeredCount, setAnsweredCount] = useState(0);
+//   const [isCompleted, setIsCompleted] = useState(false);
+
+//   const handleNextPage = () => {
+//     setPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+//     setCurrentPage("quiz");
+//   };
+
+//   const handlePrevPage = () => {
+//     setPage((prevPage) => Math.max(prevPage - 1, 0));
+//     setCurrentPage("quiz");
+//   };
+
+//   const handleOptionClick = (questionIndex, optionIndex) => {
+//     if (selectedAnswers[questionIndex] === null) {
+//       setAnsweredCount(answeredCount + 1);
+//     }
+//     const newSelectedAnswers = [...selectedAnswers];
+//     newSelectedAnswers[questionIndex] = optionIndex;
+//     setSelectedAnswers(newSelectedAnswers);
+//   };
+
+//   const calculateScore = () => {
+//     let correctAnswers = 0;
+//     selectedAnswers.forEach((selectedOption, index) => {
+//       if (
+//         selectedOption !== null &&
+//         questions[index].correctAnswer === selectedOption
+//       ) {
+//         correctAnswers++;
+//       }
+//     });
+//     return correctAnswers;
+//   };
+
+//   const handleSubmit = () => {
+//     if (selectedAnswers.includes(null)) {
+//       alert("Please answer all questions before submitting.");
+//     } else {
+//       setShowScore(true);
+//       setIsCompleted(true);
+//     }
+//   };
+//   const handleViewMissedQuestions = () => {
+//     setCurrentPage("missed-questions");
+//   };
+//   // Function to filter missed questions
+//   const getMissedQuestions = () => {
+//     return questions.filter(
+//       (q) => selectedAnswers[q.id - 1] !== q.correctAnswer
+//     );
+//   };
+//   return (
+//     <div className="Quiz-container mb-3 px-12 md:px-5 sm:px-3">
+//       <div className="fixed top-9 right-3 p-4 bg-blue-500 text-white px-2 py-1 rounded-full">
+//         {`${answeredCount}/${questions.length} answered`}
+//       </div>
+//       {currentPage !== "missed-questions" && (
+//         <div>
+//           {currentQuestions.map((q, index) => (
+//             <div key={index} className="question border p-4 rounded-lg mb-4">
+//               <h3 className="text-xl font-semibold mb-2">{q.question}</h3>
+//               <ul>
+//                 {q.options?.map((option, optionIndex) => (
+//                   <li
+//                     key={optionIndex}
+//                     className={`cursor-pointer p-2  transition-colors ${
+//                       selectedAnswers[q.id - 1] === optionIndex
+//                         ? "bg-green-500 text-white"
+//                         : "bg-gray-200"
+//                     }`}
+//                     onClick={() => handleOptionClick(q.id - 1, optionIndex)}
+//                   >
+//                     {option}
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           ))}
+//           <div className="mt-4">
+//             <button
+//               onClick={handlePrevPage}
+//               disabled={page === 0}
+//               className="mr-2 px-4 py-2 bg-blue-500 text-white rounded"
+//             >
+//               Previous Page
+//             </button>
+//             {page === totalPages - 1 ? (
+//               <button
+//                 onClick={handleSubmit}
+//                 className="px-4 py-2 bg-blue-500 text-white rounded"
+//               >
+//                 Submit
+//               </button>
+//             ) : (
+//               <button
+//                 onClick={handleNextPage}
+//                 disabled={page === totalPages - 1}
+//                 className="px-4 py-2 bg-blue-500 text-white rounded"
+//               >
+//                 Next Page
+//               </button>
+//             )}
+//           </div>
+//         </div>
+//       )}
+
+//       {isCompleted && (
+//         <div className="fixed inset-0 flex items-center justify-center bg-gray-950 bg-opacity-50">
+//           <div className="bg-blue-300 p-8 rounded shadow-lg text-center">
+//             {showScore && (
+//               <div className="mt-4">
+//                 <p className="text-3xl font-semibold text-center">
+//                   {calculateScore()} / {questions.length}
+//                 </p>
+//                 <p>
+//                   {calculateScore() === questions.length ? (
+//                     <span className="bg-gradient-to-r from-[#C25D41] via-orange-500 to-[#00C2FF] inline-block text-transparent bg-clip-text">
+//                       You answered all questions correctly!
+//                     </span>
+//                   ) : (
+//                     <span>Keep practicing to improve your score.</span>
+//                   )}
+//                 </p>
+//               </div>
+//             )}
+//             {showScore && (
+//               <button
+//                 onClick={handleViewMissedQuestions}
+//                 className="ml-2 px-4 py-2 bg-red-500 text-white rounded"
+//               >
+//                 View Missed Questions
+//               </button>
+//             )}
+//           </div>
+//         </div>
+//       )}
+
+//       {currentPage === "missed-questions" && (
+//         <MissedQuestionsPage missedQuestions={getMissedQuestions()} />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Quiz;
+
 import React, { useState } from "react";
 import MissedQuestionsPage from "./MissedQuestionsPage";
+import { Link } from "react-router-dom";
 
 function Quiz({ questions }) {
   const [currentPage, setCurrentPage] = useState("quiz");
@@ -56,15 +219,19 @@ function Quiz({ questions }) {
       setIsCompleted(true);
     }
   };
+
   const handleViewMissedQuestions = () => {
     setCurrentPage("missed-questions");
+    setIsCompleted(false);
   };
+
   // Function to filter missed questions
   const getMissedQuestions = () => {
     return questions.filter(
       (q) => selectedAnswers[q.id - 1] !== q.correctAnswer
     );
   };
+
   return (
     <div className="Quiz-container mb-3 px-12 md:px-5 sm:px-3">
       <div className="fixed top-9 right-3 p-4 bg-blue-500 text-white px-2 py-1 rounded-full">
@@ -121,7 +288,7 @@ function Quiz({ questions }) {
       )}
 
       {isCompleted && (
-        <div className="flex items-center justify-center bg-gray-950 bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-950 bg-opacity-50">
           <div className="bg-blue-300 p-8 rounded shadow-lg text-center">
             {showScore && (
               <div className="mt-4">
@@ -130,7 +297,7 @@ function Quiz({ questions }) {
                 </p>
                 <p>
                   {calculateScore() === questions.length ? (
-                    <span className="">
+                    <span className="bg-gradient-to-r from-[#C25D41] via-orange-500 to-[#00C2FF] inline-block text-transparent bg-clip-text">
                       You answered all questions correctly!
                     </span>
                   ) : (
@@ -139,13 +306,17 @@ function Quiz({ questions }) {
                 </p>
               </div>
             )}
-            {showScore && (
+            {showScore && calculateScore() !== questions.length ? (
               <button
                 onClick={handleViewMissedQuestions}
                 className="ml-2 px-4 py-2 bg-red-500 text-white rounded"
               >
                 View Missed Questions
               </button>
+            ) : (
+              <Link to="/" className="text-blue-400 mt-2">
+                Go back to Homepage
+              </Link>
             )}
           </div>
         </div>
