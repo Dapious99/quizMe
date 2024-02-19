e6826e7610e052ef382648ef2b82ffd08f63d972
Omotosho Kamalideen <dapious99@gmail.com>
2024-02-19 08:01:59 +0100

    little cleanup and addition

--- quizApp/src/views/homepage/Home.jsx
+++ quizApp/src/views/homepage/Home.jsx
@@ -5,9 +5,11 @@ const Home = () => {
   return (
     <div className="flex justify-center items-center bg-white text-black h-screen">
       <div>
-        <p className="animate__animated animate__flip animate__infinite animate__slower font-semibold lg:font-bold text-2xl py-6 lg:text-[3rem] relative left-80 bg-gradient-to-r from-blue-400 via-red-500 to-blue-400 inline-block text-transparent bg-clip-text">
-          quizMe
-        </p>
+        <div className="flex justify-center">
+          <p className="animate__animated animate__flip animate__infinite animate__slower font-semibold lg:font-bold text-2xl py-6 lg:text-[3rem] bg-gradient-to-r from-blue-400 via-red-500 to-blue-400 inline-block text-transparent bg-clip-text">
+            quizMe
+          </p>
+        </div>
         <p className="lg:text-[2rem] text-2xl text-center opacity-70">
           Welcome to{" "}
           <span className="italic text-blue-700 text-[2rem] lg:text-[2.5rem]">
