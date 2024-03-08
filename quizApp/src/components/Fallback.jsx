bd2150c095f35b83a3025bc8a8130990d808bc7f
Omotosho Kamalideen <dapious99@gmail.com>
2024-03-08 05:33:39 +0100

    adjustment on the fallback component

--- quizApp/src/components/Fallback.jsx
+++ quizApp/src/components/Fallback.jsx
@@ -6,7 +6,8 @@ const Fallback = () => {
     <div className="flex flex-col items-center bg-gray-200 h-screen justify-center">
       <h1 className="text-5xl text-red-800">Error 404</h1>
       <p className="text-xl text-neutral-600">
-        The page or resource you are requesting is not available.
+        The page or resource you are requesting is not available or may have
+        moved completely.
       </p>
       <Link to="/" className="text-blue-400">
         Go back to home
