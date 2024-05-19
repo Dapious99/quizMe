3d807b118822a7c8cd02d70f82260a0befedacf0
Omotosho Kamalideen <dapious99@gmail.com>
2024-05-19 13:40:04 +0100

    added some word to fallback component

--- quizApp/src/components/Fallback.jsx
+++ quizApp/src/components/Fallback.jsx
@@ -7,7 +7,7 @@ const Fallback = () => {
       <h1 className="text-5xl text-red-800">Error 404</h1>
       <p className="text-xl text-neutral-600">
         The page or resource you are requesting is not available or may have
-        moved completely.
+        moved completely, check and confirm the url.
       </p>
       <Link to="/" className="text-blue-400">
         Go back to home
